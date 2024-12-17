1a723dfbd7e3d793352aec1fd36ea16cf1b8a683
blafed <a125b25@gmail.com>
2024-12-17 15:18:52 +0200

    basics (not working)

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// #include "lib\\quickjs\\quickjs.h"
+
+// void main(int args){
+//     JSRuntime* runtime = JS_NewRuntime();
+//     JSContext* context = JS_NewContext(runtime);
+
+    
+
+
+//     printf("hello world");
+// }
